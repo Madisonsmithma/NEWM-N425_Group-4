86c327ff319f66160e710a16f5aec85e1c786419
Madisonsmithma <126831719+Madisonsmithma@users.noreply.github.com>
2023-06-21 21:07:18 -0400

    Update createMenuItem.js

--- mcdonald-client/src/pages/menuitem/createMenuItem.js
+++ mcdonald-client/src/pages/menuitem/createMenuItem.js
@@ -5,19 +5,92 @@ import {useForm} from "react-hook-form";
 import JSONPretty from 'react-json-pretty';
 import "./menuitem.css";
 
-const CreateMenuItem = () => {
+import React from 'react';
 
+const CreateMenuItem = ({showModal, setShowModal, reload, setReload, setSubHeading}) => {
+    const {error, isLoading, data: response, create} = UseFetch();
+    const [submitted, setSubmitted] = useState(false);
+    const [showButton, setShowButton] = useState(true);
+    
+   const {register, handleSubmit, formState: {errors}} = useForm({
+        defaultValues: {id: "", name: "", email: ""},
+        shouldUseNativeValidation: false
+    });
+    const createFormOptions = {
+        id: {required: "ID is required"},
+        name: {required: "Name is required"},
+        email: {required: "Email is required"}
+    }
+
+    const handleCreate = (menuitem) => {
+        create(menuitem);
+        setSubmitted(true);
+    }
+    const handleCancel = () => {
+        setShowModal(false);
+        setSubHeading("All Menu Items");
+    }
+    const handleClose = () => {
+        setShowModal(false);
+        setShowButton(true);
+        setSubmitted(false);
+        setReload(!reload);
+        setSubHeading("All Menu Items");
+    }
+    useEffect(() => {
+        if (!submitted || error != null) {
+            setShowButton(true);
+        } else {
+            setShowButton(false);
+        }
+    })
     return (
-        <>
-            <h2 className="crud-form-title">Create New Menu Item</h2>
-            <form className="crud-form">
-                <input type="number" placeholder="ID"></input>
-                <input type="text" placeholder="Name"></input>
-                <input type="text" placeholder="Description"></input>
-                <input type="number" step="0.01" placeholder="Price"></input>
-                <button type="submit">Submit</button>
-            </form>
-        </>
+        <div>
+            <Modal show={showModal} onHide={handleClose} centered animation={false} backdrop="static">
+                <Modal.Header closeButton>
+                    <h4>Create Menu Item</h4>
+                </Modal.Header>
+                <Modal.Body>
+                    {error && <JSONPretty data={error} style={{color: "red"}}></JSONPretty>}
+                    {isLoading &&
+                        <div className="image-loading">
+                            Please wait while data is being loaded
+                            <img src={require(`../loading.gif`)} alt="Loading ......"/>
+                        </div>
+                    }
+                    {response && <JSONPretty data={response}></JSONPretty>}
+                    {(!submitted || error != null) &&
+                        <form className="form-student" id="form-student-edit" onSubmit={handleSubmit(handleCreate)}>
+                            <ul className="form-student-errors">
+                                {errors?.id && <li>{errors.id.message}</li>}
+                                {errors?.name && <li>{errors.name.message}</li>}
+                                {errors?.email && <li>{errors.email.message}</li>}
+                            </ul>
+                            <div className="form-group">
+                                <label>Student ID</label>
+                                <input name="id" {...register('id', createFormOptions.id)}/>
+                            </div>
+                            <div className="form-group">
+                                <label>Name</label>
+                                <input type="text" name="name" {...register('name', createFormOptions.name)}/>
+                            </div>
+                            <div className="form-group">
+                                <label>Email</label>
+                                <input name="email" {...register('email', createFormOptions.email)}/>
+                            </div>
+                        </form>
+                    }
+                </Modal.Body>
+                <Modal.Footer style={{justifyContent: "center"}}>
+                    <Button variant="primary" form="form-menuitem-edit" type="submit"
+                            style={{display: (!showButton) ? "none" : ""}}>Create</Button>
+                    <Button variant="secondary" onClick={handleCancel}
+                            style={{display: (!showButton) ? "none" : ""}}>Cancel</Button>
+                    <Button variant="primary" onClick={handleClose}
+                            style={{display: (!showButton) ? "" : "none"}}>Close</Button>
+                </Modal.Footer>
+            </Modal>
+        </div>
     );
 };
 
